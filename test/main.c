51bb14a7548ddc2a5af2a462ff000cbec7f3bab7
faye <faye@faye.com>
2019-10-08 17:12:58 +0800

    master@T430 191008 modified test/main.c

--- test/main.c
+++ test/main.c
@@ -12,7 +12,7 @@ int main( int argc, char argv[] ){
     int fd;
     
     fd = 0;
-    fd = open( "/dev/globalMem_device_0", O_RDWR, S_IRUSR | S_IWUSR );
+    fd = open( "/dev/globalMem_device", O_RDWR, S_IRUSR | S_IWUSR );
     if( fd <=0 ){
         perror( "open file error!\n" );
         return -1;
@@ -20,17 +20,20 @@ int main( int argc, char argv[] ){
 
     memset( cBuf, 0, sizeof(cBuf) );
     read( fd, cBuf, sizeof(cBuf) );
-    printf( "in /dev/globalMem_device_0:\n%s\n", cBuf );
+    printf( "in /dev/globalMem_device:\n%s\n", cBuf );
 
     memset( cBuf, 0, sizeof(cBuf) );
     printf( "please input your string:\n" );
     scanf( "%s", cBuf );
     write( fd, cBuf, (strlen(cBuf)) );
 
+    /*
+     * 通过ioctl()方法设置本字符设备的当前读写位置，最终会调用cdev对象文件操作表中的
+     * unlocked_ioctl()函数（cdev.ops->unlocked_ioctl()字段） */
     ioctl( fd, 0, 0 );
     memset( cBuf, 0, sizeof(cBuf) );
     read( fd, cBuf, sizeof(cBuf) );
-    printf( "in /dev/globalMem_device_0:\n%s\n", cBuf );
+    printf( "in /dev/globalMem_device:\n%s\n", cBuf );
 
     close( fd );
 
